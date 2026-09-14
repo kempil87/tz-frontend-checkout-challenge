58d3fc9e579bcd2899d28f9b9673fa9d6174c04a
Kempil87 <glebxok@mail.ru>
2026-09-14 03:12:00 +0300

    [fix]: rename file

---
+++ apps/web/src/shared/lib/amount-format.ts
@@ -0,0 +1,6 @@
+export const amountFormat = (value: number) =>
+  new Intl.NumberFormat('ru-RU', {
+    style: 'currency',
+    currency: 'RUB',
+    maximumFractionDigits: 0,
+  }).format(value / 100);
